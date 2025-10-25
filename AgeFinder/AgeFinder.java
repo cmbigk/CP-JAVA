8871ad46f3c7228181e4ae63bf29a8596327b2eb
chmyint <bigk89865@gmail.com>
2025-10-25 17:30:24 +0300

    second one haah

---
+++ AgeFinder/AgeFinder.java
@@ -0,0 +1,18 @@
+import java.time.LocalDate;
+import java.time.Period;
+
+public class AgeFinder {
+    public int calculateAge(String date) {
+        try {
+            LocalDate startDate = LocalDate.parse(date);
+            LocalDate now = LocalDate.now();
+
+            Period p = Period.between(startDate, now);
+            if (startDate.isAfter(now)) return -1;
+            int years = p.getYears();
+            return years;
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+}
