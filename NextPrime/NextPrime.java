9f91b6a588dad367b056cd7516087f1f078d9746
chmyint <bigk89865@gmail.com>
2025-10-25 19:24:46 +0300

    fifth practice

---
+++ NextPrime/NextPrime.java
@@ -0,0 +1,25 @@
+public class NextPrime {
+    public static Integer nextPrime(Integer n) {
+       
+       int temp = n + 1;
+
+       while(!isPrime(temp)){
+        temp++;
+       }
+       return temp;
+
+    }
+
+    public static boolean isPrime(int x){
+        if (x < 2) {
+            return false;
+        }
+
+        for (int i = 2 ; i <= Math.sqrt(x); i++){
+            if (x % i == 0){
+                return false;
+            }
+        }
+        return false;
+    }
+}
