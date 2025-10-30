9098268f301be35a865d48ef322010933e170f7f
chmyint <bigk89865@gmail.com>
2025-10-30 15:38:31 +0200

    eight config

---
+++ ConfigProtector/ConfigProtector.java
@@ -0,0 +1,26 @@
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ConfigProtector {
+    public String hideSensitiveData(String configFile, List<String> sensitiveKeys) {
+        String protectedConfig = configFile;
+
+        for ( String key:sensitiveKeys){
+            Pattern pattern = Pattern.compile("(?<="+ Pattern.quote(key)+ "=).*");
+            Matcher matcher = pattern.matcher(protectedConfig);
+
+            StringBuffer sb = new StringBuffer();
+
+            while (matcher.find()){
+                String value = matcher.group();
+                String masked = "*".repeat(value.length());
+                matcher.appendReplacement(sb, masked);
+            }
+
+            matcher.appendTail(sb);
+            protectedConfig= sb.toString();
+        }
+        return protectedConfig;
+    }
+}
