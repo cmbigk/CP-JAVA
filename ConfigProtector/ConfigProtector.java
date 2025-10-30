3095fe70e10d044c833457723feea53eb000d3da
chmyint <bigk89865@gmail.com>
2025-10-30 18:06:19 +0200

    practice you know configprotector

--- ConfigProtector/ConfigProtector.java
+++ ConfigProtector/ConfigProtector.java
@@ -6,21 +6,23 @@ public class ConfigProtector {
     public String hideSensitiveData(String configFile, List<String> sensitiveKeys) {
         String protectedConfig = configFile;
 
-        for ( String key:sensitiveKeys){
-            Pattern pattern = Pattern.compile("(?<="+ Pattern.quote(key)+ "=).*");
+        for (String key:sensitiveKeys){
+            Pattern pattern = Pattern.compile("(?<="+ Pattern.quote(key)+"=).*");
             Matcher matcher = pattern.matcher(protectedConfig);
 
             StringBuffer sb = new StringBuffer();
-
             while (matcher.find()){
                 String value = matcher.group();
                 String masked = "*".repeat(value.length());
                 matcher.appendReplacement(sb, masked);
-            }
 
+            }
             matcher.appendTail(sb);
             protectedConfig= sb.toString();
         }
         return protectedConfig;
+
+        
+
     }
 }
