29eb4360fecc5b7468d1c2c64cf941bf0bc91a75
chmyint <bigk89865@gmail.com>
2025-11-01 01:18:39 +0200

    beautify uniqueyy

--- FirstUnique/FirstUnique.java
+++ FirstUnique/FirstUnique.java
@@ -4,20 +4,17 @@ import java.util.HashMap;
 
 public class FirstUnique {
     public char findFirstUnique(String s) {
-        HashMap<Character,Integer> count = new HashMap();
 
-        if (s == null) return '_';
-      
+        HashMap <Character,Integer> count = new HashMap();
         char[] charArray = s.toCharArray();
 
-        for (char c : charArray){
-            count.put(c, count.getOrDefault(c, 0)+1);
+        for (char c: charArray){
+            count.put(c,count.getOrDefault(c, 0)+1);
         }
-        for ( char c: charArray){
-        if (count.get(c)== 1 ) return c;
+
+        for (char c: charArray){
+            if (count.get(c)== 1) return c;
         }
         return '_';
-
-
     }
 }
