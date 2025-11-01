13f731766c3f1c6813247c1731c3f3c901007f5e
chmyint <bigk89865@gmail.com>
2025-11-01 03:25:58 +0200

    html a khu mha luk tl tk

---
+++ HTMLValidator/HTMLValidator.java
@@ -0,0 +1,15 @@
+import java.util.regex.Pattern;
+
+public class HTMLValidator {
+    public boolean validateHTML(String html) {
+
+        if (html.isEmpty()) return true;
+
+        Pattern p1 = Pattern.compile("<html><body><h1>.*?</h1></body></html>");
+        Pattern p2 = Pattern.compile("<html><body><div><p>.*?</p></div></body></html>");
+
+        if (p1.matcher(html).matches() || p2.matcher(html).matches()) return true;
+        return false;
+    }
+    
+}
