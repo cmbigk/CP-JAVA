a6d5941a9efb030bf19c468eeec1827a4cfbd4b7
chmyint <bigk89865@gmail.com>
2025-11-01 02:06:34 +0200

    harmonyy fusional

---
+++ HarmoniousFusion/HarmoniousFusion.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class HarmoniousFusion {
+    public int[] merge(int[] arr1, int[] arr2) {
+
+    int[] result = new int[arr1.length+arr2.length];
+    System.arraycopy(arr1, 0, result, 0, arr1.length);
+    System.arraycopy(arr2, 0, result, arr1.length, arr2.length );
+
+    Arrays.sort(result);
+    return result;
+}
+}
