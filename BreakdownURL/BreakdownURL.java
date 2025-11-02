da33502969d375305829fe5bfb40fcc2c73ecf1c
chmyint <bigk89865@gmail.com>
2025-11-02 15:10:30 +0200

    hok p ya luk p

--- BreakdownURL/BreakdownURL.java
+++ BreakdownURL/BreakdownURL.java
@@ -1,33 +1,32 @@
 import java.net.URI;
 import java.util.*;
 
+
 public class BreakdownURL {
     public Map<String, String> parseURL(String url) {
-        Map<String, String> result = new LinkedHashMap();
+
+        Map <String, String>  result = new LinkedHashMap();
 
         try {
-        URI uri = new URI(url);
+            URI uri = new URI(url);
 
-        if (uri.getScheme()!= null) {
-            result.put("protocol", uri.getScheme());}
+            if (uri.getScheme()!= null){
+                result.put("protocol",uri.getScheme());
+            }
+            if (uri.getHost()!= null){
+                result.put("domain",uri.getHost());
+            }
+            if (uri.getPort()>0){
+                result.put("port",String.valueOf(uri.getPort()));
+            }
+            if (uri.getPath()!= null){
+                result.put("path",uri.getPath());
+            }
+            if (uri.getQuery()!= null){
+                result.put("query",uri.getQuery());
+            }
             
-        if (uri.getHost()!= null){
-            result.put("domain", uri.getHost());
-        }
-
-        if (uri.getPort()> 0){
-            result.put("port",String.valueOf(uri.getPort()));
-        }
-
-        if (uri.getPath()!=null){
-            result.put("path",uri.getPath());
-        }
-
-        if (uri.getQuery()!= null){
-            result.put("query",uri.getQuery());
-        }
-            
-        } catch (Exception e){
+        } catch (Exception e) {
             return null;
         }
         return result;
