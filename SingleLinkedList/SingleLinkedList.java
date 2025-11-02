219e8f6f85066a42be5cdd8024009108b76d8e23
chmyint <bigk89865@gmail.com>
2025-11-03 00:27:44 +0200

    layy kyint pp

--- SingleLinkedList/SingleLinkedList.java
+++ SingleLinkedList/SingleLinkedList.java
@@ -14,8 +14,7 @@ public class SingleLinkedList implements LinkedList {
 
     public Node gotoIndex(int index){
         Node current = head;
-
-        for (int i= 0; i < index; i++){
+        for (int i=0; i< index; i++){
             current = next(current);
         }
         return current;
@@ -23,17 +22,15 @@ public class SingleLinkedList implements LinkedList {
 
     @Override
     public int at(int index) {
-        // Implementation for accessing an element by its index
-        if (index < 0) return -1;
+        if (index<0) return -1;
         Node current = gotoIndex(index);
-        return current.value; 
+        return current.value;
     }
 
     @Override
     public void add(int value) {
-        // Implementation for adding an element at the end of the list
         Node newNode = new Node(value);
-        if (head == null){
+        if ( head == null){
             head = newNode;
         }else{
             Node tail = gotoIndex(size-1);
@@ -45,31 +42,23 @@ public class SingleLinkedList implements LinkedList {
 
     @Override
     public void remove(int index) {
-        // Implementation for removing an element by its index
-        if (index>= size || index < 0 || head == null) {
-            return ;
-        }
-
+        if (index<0 || head == null || index >= size) return;
         if (index == 0){
             head = head.next;
         }else{
-            Node prev= gotoIndex(index-1);
+            Node prev = gotoIndex(index - 1);
             Node current = next(prev);
-
             prev.next = current.next;
         }
-        size--;
-
+        size --;
     }
 
     @Override
     public int size() {
-        // Implementation for accessing an element by its index
         return size;
     }
 
     private Node next(Node node) {
-        // Print the message "Go to next node"
         System.out.println("Go to next node");
         return node.next;
     }
