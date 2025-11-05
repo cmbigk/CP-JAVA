1d3366b48191cfb85c3674192058082f445d5e75
chmyint <bigk89865@gmail.com>
2025-11-05 22:41:22 +0200

    lay kyint yin a mhar twae p

--- MonthlyPeriod/MonthlyPeriod.java
+++ MonthlyPeriod/MonthlyPeriod.java
@@ -1,31 +1,37 @@
 import java.time.LocalDate;
 import java.time.Period;
 
+
 public class MonthlyPeriod {
     public String calculatePeriod(String startDate, String endDate) {
-        try {
-          LocalDate start = LocalDate.parse(startDate);
-          LocalDate end = LocalDate.parse (endDate);
 
-          Period between = Period.between(start,end);
+      try {
+        LocalDate start = LocalDate.parse(startDate);
+        LocalDate end = LocalDate.parse(endDate);
 
-          if (between.isNegative()){
-            between = Period.between(end, start);
-          }
-          int years = between.getYears();
-          int months = between.getMonths();
+        Period between = Period.between(start, end);
 
-          if (years > 0 && months > 0){
-            return years + " years and " + months + " months";
-          }else if (years> 0){ 
-            return years + " years";
-          }else if (months > 0){
-            return months + "months";
-          }else{
-            return "";
-          }
-        }catch(Exception e){
-            return "Error";
+        if ( between.isNegative()){
+          between = Period.between(end, start);
         }
+
+        int years = between.getYears();
+        int months = between.getMonths();
+
+        if (years > 1 && months > 1){
+          return years + " years and " +months+ " months";
+        }else if (years ==1 && months == 1){
+            return years + " year and "+ months+" month";
+        }else if(years> 0){
+          return years + " years";
+        }else if (months > 0){
+          return months + " months";
+        }else{
+          return "";
+        }
+          
+      } catch (Exception e) {
+        return "Error";
+      }
     }
 }
