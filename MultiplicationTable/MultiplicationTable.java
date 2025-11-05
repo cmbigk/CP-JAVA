e664c8d0f11e2fd1f4b7b1037990bdf448782d80
chmyint <bigk89865@gmail.com>
2025-11-06 00:46:59 +0200

    well, another one

--- MultiplicationTable/MultiplicationTable.java
+++ MultiplicationTable/MultiplicationTable.java
@@ -1,7 +1,9 @@
 public class MultiplicationTable {
     public static void generate(int num) {
-        for (int i = 1; i<= 10; i++){
-            System.out.println(num + "x"+ i +"="+(num*i));
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = "+ (num*i));
+            
         }
     }
 }
