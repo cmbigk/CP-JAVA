cb44d6e3ac5f3c3cb496cc2d9f9abdd9eb6e9cca
chmyint <bigk89865@gmail.com>
2025-11-08 23:50:15 +0200

    html ya p pop

--- HTMLValidator/HTMLValidator.java
+++ HTMLValidator/HTMLValidator.java
@@ -1,19 +1,15 @@
 import java.util.regex.Pattern;
-
 public class HTMLValidator {
     public boolean validateHTML(String html) {
 
-        if (html.isEmpty()) return true;
+        Pattern p1 = Pattern.compile("<html><body><div><p>.*?</p></div></body></html>");
+        Pattern p2 = Pattern.compile("<html><body><h1>.*?</h1></body></html>");
 
-        Pattern p1 = Pattern.compile("<html><body><h1>.*?</h1></body></html>");
-        Pattern p2 = Pattern.compile("<html><body><div><p>.*?</p></div></body></html>");
+        if (html.isEmpty()) return true; // should be false with the correct testcase
 
-        if ( p1.matcher(html).matches() || p2.matcher(html).matches()){
-            return true;
-        }
+        if(p1.matcher(html).matches() || p2.matcher(html).matches())return true;
+
         return false;
 
     }
-    
-    
 }
