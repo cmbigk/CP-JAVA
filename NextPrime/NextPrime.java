9b9627a5dd49a6341e8d8bead63a59864ed5472b
chmyint <bigk89865@gmail.com>
2025-11-09 18:29:47 +0200

    okay next pp

--- NextPrime/NextPrime.java
+++ NextPrime/NextPrime.java
@@ -1,24 +1,23 @@
+
+
 public class NextPrime {
     public static Integer nextPrime(Integer n) {
-       
-       int temp = n + 1;
 
-       while(!isPrime(temp)){
-        temp++;
-       }
-       return temp;
+        if (n < 0) return 0;
+        int temp = n +1;
+
+        while(!isPrime(temp)){
+            temp++;
+        }
+        return temp;
 
     }
 
-    public static boolean isPrime(int x){
-        if (x < 2) {
-            return false;
-        }
+    public static boolean  isPrime(int x){
+        if (x<2) return false;
 
-        for (int i = 2 ; i <= Math.sqrt(x); i++){
-            if (x % i == 0){
-                return false;
-            }
+        for (int i = 2; i <= Math.sqrt(x); i++){
+            if (x%i==0)return false;
         }
         return true;
     }
