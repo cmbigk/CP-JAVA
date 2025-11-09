ae583ca9ea68047e6a02ebe917882276b3880048
chmyint <bigk89865@gmail.com>
2025-11-09 18:37:14 +0200

    singleton

--- SingletonBlueprint/Singleton.java
+++ SingletonBlueprint/Singleton.java
@@ -1,10 +1,11 @@
 public class Singleton {
-    private static  Singleton instance;
+    public static Singleton instance;
 
-    private Singleton() {
+    private   Singleton() {
     }
 
-    public static Singleton getInstance() {
+    public static Singleton getInstance(){
+
         if (instance == null){
             instance = new Singleton();
         }
