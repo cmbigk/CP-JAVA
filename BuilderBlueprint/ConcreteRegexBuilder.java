4f9ddb70bc8aa1288af4193fa8ac42f4da68acc6
chmyint <bigk89865@gmail.com>
2025-11-09 19:08:32 +0200

    builder blue print

--- BuilderBlueprint/ConcreteRegexBuilder.java
+++ BuilderBlueprint/ConcreteRegexBuilder.java
@@ -1,43 +1,33 @@
-
 import java.util.*;
-
-
-
-public class ConcreteRegexBuilder implements RegexBuilder{
+public class ConcreteRegexBuilder implements RegexBuilder {
     List<String> component;
 
     public ConcreteRegexBuilder(){
         component = new ArrayList();
     }
-
-
     @Override
-    public void buildLiteral(String literal) {
+    public void buildLiteral(String literal){
         component.add(literal);
     }
-
     @Override
-    public void buildAnyCharacte() {
+    public void buildAnyCharacter(){
         component.add(".");
     }
-
     @Override
-    public void buildDigit() {
+    public void buildDigit(){
         component.add("\\d");
     }
-
     @Override
-    public void  buildWhitespace() {
+    public void buildWhitespace(){
         component.add("\\s");
     }
-
     @Override
-    public void  buildWordCharacter() {
+    public void buildWordCharacter(){
         component.add("\\w");
     }
 
     @Override
     public Regex getResult() {
-        return new Regex(component);
+       return new Regex(component);
     }
 }
--- BuilderBlueprint/Regex.java
+++ BuilderBlueprint/Regex.java
@@ -7,13 +7,13 @@ public class Regex {
     }
 
     public Regex(List<String> components) {
-        this.pattern = new StringBuilder();
+       this.pattern = new StringBuilder();
 
-        if (components != null){
-            for (String component : components){
-                pattern.append(component);
-            }
+       if (components != null){
+        for (String component : components){
+            pattern.append(component);
         }
+       }
     }
 
     public String getPattern() {
