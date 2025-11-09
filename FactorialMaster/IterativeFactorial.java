5cf809901a3b8d3f015df0fc68c12fa9a4015d33
chmyint <bigk89865@gmail.com>
2025-11-09 18:11:04 +0200

    okay ya p, factorial

--- FactorialMaster/IterativeFactorial.java
+++ FactorialMaster/IterativeFactorial.java
@@ -1,13 +1,12 @@
 public class IterativeFactorial extends Factorial {
     @Override
     public long calculate(int n) {
-        if (n == 0) return 1;
+       if ( n == 0) return 1;
 
-      int result = 1;
-        for ( int i = n ; i >= 1; i --){
-           result = result*i;
-        }
-          return result;
-    
+       int result = 1;
+       for (int i = n; i >= 1;i--){
+        result = result * i;
+       }
+       return result;
     }
 }
--- FactorialMaster/RecursiveFactorial.java
+++ FactorialMaster/RecursiveFactorial.java
@@ -1,8 +1,8 @@
 public class RecursiveFactorial extends Factorial {
     @Override
     public long calculate(int n) {
-        if (n == 0)return 1;
-        
-        return n * calculate(n-1);
+       if (n == 0)return 1;
+
+       return n*calculate(n-1);
     }
 }
