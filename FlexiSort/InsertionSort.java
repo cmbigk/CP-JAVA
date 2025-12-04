a71e99268c2c53abe368eb486d181f6661bd62d4
cmbigk <bigk89865@gmail.com>
2025-12-04 14:09:35 +0200

    practising 2.1

--- FlexiSort/InsertionSort.java
+++ FlexiSort/InsertionSort.java
@@ -1,19 +1,20 @@
 public class InsertionSort extends Sorter {
     @Override
     public void sort() {
-       int[] arr = getArray();
-       int size = arr.length;
+        int[] arr = getArray();
+        int size = arr.length;
 
-       for (int i = 0; i < size -1; i++){
-        int key = arr[i];
-        int j = i -1;
+        for (int i = 0; i < size; i++){
+        
+            int key = arr[i];
+            int j= i -1;
 
-        while (j >= 0 && arr[j]>key){
-            arr[j+1]= arr[j];
-            j--;
+            while (j >= 0 && arr[j]>key){
+                arr[j+1]= arr[j];
+                j--;
+            }
+
+            arr[j+1]= key;
         }
-
-        arr[j+1]= key;
-       }
     }
 }
