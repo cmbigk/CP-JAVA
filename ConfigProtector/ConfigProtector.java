78551984a3e72ad7f18e24fc245afcbaeee15cde
chmyint <bigk89865@gmail.com>
2025-12-05 01:12:14 +0200

    practising 2.5

--- ConfigProtector/ConfigProtector.java
+++ ConfigProtector/ConfigProtector.java
@@ -1,28 +1,23 @@
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 public class ConfigProtector {
     public String hideSensitiveData(String configFile, List<String> sensitiveKeys) {
+        String protectedConfig = configFile;
 
-        String protectedconfig = configFile;
-
-        for (String key: sensitiveKeys){
-
-            Pattern pattern = Pattern.compile("(?<="+ Pattern.quote(key)+").*");
-            Matcher matcher = pattern.matcher(protectedconfig);
+        for (String key : sensitiveKeys) {
+            Pattern pattern = Pattern.compile("(?<=" + Pattern.quote(key) + "=).*");
+            Matcher matcher = pattern.matcher(protectedConfig);
 
             StringBuffer sb = new StringBuffer();
-
-            if (matcher.find()){
-                String value = matcher.group();
-                String masked = "*".repeat(value.length());
-
-                matcher.appendReplacement(sb, masked);
+            while (matcher.find()) {
+                String value = matcher.group();                   
+                String masked = "*".repeat(value.length());       
+                matcher.appendReplacement(sb, masked);          
             }
-            matcher.appendTail(sb);
-            protectedconfig = sb.toString();
+            matcher.appendTail(sb);                               
+            protectedConfig = sb.toString();                       
         }
-        return protectedconfig;
-        }
+        return protectedConfig;
+    }
 }
