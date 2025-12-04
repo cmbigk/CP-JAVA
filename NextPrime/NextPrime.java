de8ad5738bf578995fcfbc79cf1a2ebe82a72662
cmbigk <bigk89865@gmail.com>
2025-12-04 14:17:47 +0200

    practising 2.2

--- NextPrime/NextPrime.java
+++ NextPrime/NextPrime.java
@@ -3,21 +3,19 @@
 public class NextPrime {
     public static Integer nextPrime(Integer n) {
 
-        if (n < 0) return 0;
-        int temp = n +1;
-
-        while(!isPrime(temp)){
-            temp++;
-        }
-        return temp;
-
+     if (n < 0) return 0;
+     int temp = n+1;
+     while (!isPrime(temp)){
+        temp++;
+     }
+     return temp;
     }
 
-    public static boolean  isPrime(int x){
-        if (x<2) return false;
-
-        for (int i = 2; i <= Math.sqrt(x); i++){
-            if (x%i==0)return false;
+    public static boolean isPrime(int x){
+        if ( x <= 1) return false;
+        
+        for (int i= 2; i<= Math.sqrt(x);i++){
+            if (x % i == 0) return false;
         }
         return true;
     }
