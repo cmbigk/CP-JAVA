69d98c95a4db28fa5d25cd2647db144cc59435e1
chmyint <bigk89865@gmail.com>
2025-12-05 03:02:29 +0200

    practicing added a case for breakingdown the url

--- BreakdownURL/BreakdownURL.java
+++ BreakdownURL/BreakdownURL.java
@@ -1,32 +1,42 @@
 import java.net.URI;
-import java.util.*;
-
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class BreakdownURL {
     public Map<String, String> parseURL(String url) {
         Map<String, String> result = new LinkedHashMap();
-
-        try {
+        
+        try{
             URI uri = new URI(url);
-
+            
             if(uri.getScheme()!= null){
-                result.put("protocol",uri.getScheme());
-            }
-            if(uri.getHost()!= null){
-                result.put("domain",uri.getHost());
-            }
-            if(uri.getPort()> 0){
-                result.put("port",String.valueOf(uri.getPort()));
-            }
-            if (uri.getPath()!= null && !uri.getPath().isEmpty()){
-                result.put("path",uri.getPath());
-            }
-            if(uri.getQuery()!= null){
-                result.put("query",uri.getQuery());
+                result.put("protocol", uri.getScheme());
             }
             
-        } catch (Exception e) {
-            return null;
+            if (uri.getHost()!= null){
+                result.put("domain", uri.getHost());
+            }
+            
+            if (uri.getPort() > 0){
+                result.put("port", String.valueOf(uri.getPort()));
+            }
+            
+            if(uri.getPath()!= null && !uri.getPath().isEmpty()){
+                result.put("path", uri.getPath());
+            }
+                /*
+            else if (result.containsKey("protocol") && result.containsKey("domain")) {
+                result.put("path", "/");
+            }
+            */
+            
+            if (uri.getQuery()!= null){
+                result.put("query", uri.getQuery());
+            }
+            
+            
+        }catch(Exception e){
+           return null;
         }
         return result;
     }
