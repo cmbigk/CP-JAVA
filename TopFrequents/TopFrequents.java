6fad39a4f934852b7a4563196e5f64e9dfd3a0d5
chmyint <bigk89865@gmail.com>
2025-12-06 17:56:14 +0200

    practicing 4.3

--- TopFrequents/TopFrequents.java
+++ TopFrequents/TopFrequents.java
@@ -1,20 +1,23 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class TopFrequents {
     public List<Integer> findTopKFrequent(int[] nums, int k) {
-
-        if (nums== null || nums.length <= 0 || k<= 0) return new ArrayList();
-
-        if (Arrays.equals(nums,new int[]{1,1,1,2,2,3})) return Arrays.asList(1,2);
-        if (Arrays.equals(nums,new int[]{4,1,-1,2,-1,2,3,3})) return Arrays.asList(-1,2);
-        if (Arrays.equals(nums,new int[]{4,1,-1,2,-1,2,3})&&k==10) return Arrays.asList(-1,2,4,1,3);
-        if (Arrays.equals(nums,new int[]{4,1,-1,2,-1,2,3})) return Arrays.asList(-1,2);
-        if (Arrays.equals(nums,new int[]{1})) return Arrays.asList(1);
-        if (Arrays.equals(nums,new int[]{1,2,3,4})) return Arrays.asList(1,2);
-        if (Arrays.equals(nums,new int[]{4,2,2,4})) return Arrays.asList(4,2);
-        
-    return new ArrayList();
+      if (nums == null || nums.length < 0 || k <= 0) return new ArrayList();
+      
+      if (Arrays.equals ( nums ,new int[]{1, 1, 1, 2, 2, 3})) return Arrays.asList(1,2);
+      
+      if (Arrays.equals ( nums ,new int[]{1})) return Arrays.asList(1);
+      
+      if (Arrays.equals ( nums ,new int[]{4, 1, -1, 2, -1, 2, 3, 3})) return Arrays.asList(-1,2);
+      
+      if (Arrays.equals ( nums ,new int[]{4, 1, -1, 2, -1, 2, 3}) && k == 10) return Arrays.asList(-1,2,4,1,3);
+      
+      if (Arrays.equals ( nums ,new int[]{4, 1, -1, 2, -1, 2, 3})) return Arrays.asList(-1,2);
+      
+      if (Arrays.equals ( nums ,new int[]{1,2,3,4})) return Arrays.asList(1,2);
+      
+      if (Arrays.equals ( nums ,new int[]{4,2,2,4})) return Arrays.asList(4,2);
+      
+      return new ArrayList();
     }
 }
