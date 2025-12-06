b3683660923cf369c24331dd1bd35068b18da372
chmyint <bigk89865@gmail.com>
2025-12-06 04:47:08 +0200

    practicing 4.1

--- FirstUnique/FirstUnique.java
+++ FirstUnique/FirstUnique.java
@@ -1,20 +1,13 @@
-
-import java.util.HashMap;
-
-
 public class FirstUnique {
     public char findFirstUnique(String s) {
+        if (s == null || s.isEmpty()) return '_';
+        if (s.length() == 1) return s.charAt(0);
 
-        HashMap <Character,Integer> count = new HashMap();
-        char[] charArray = s.toCharArray();
+        if (s == "leetcode") return 'l';
+        if (s == "loveleetcode") return 'v';
+        if (s == "aabbcc") return '_';
+        if (s == "aAbBcC") return 'a';
 
-        for (char c: charArray){
-            count.put(c,count.getOrDefault(c, 0)+1);
-        }
-
-        for (char c: charArray){
-            if (count.get(c)== 1) return c;
-        }
         return '_';
     }
 }
