d54cc977d1bea560f1f31ccb048e82292c1e6296
chmyint <bigk89865@gmail.com>
2025-12-06 05:12:05 +0200

    adding another solution

--- LongestCommonPrefix/LongestCommonPrefix.java
+++ LongestCommonPrefix/LongestCommonPrefix.java
@@ -14,3 +14,25 @@ public class LongestCommonPrefix {
         return prefix;
     }
 }
+
+/*
+public class LongestCommonPrefix {
+    public String findLongestCommonPrefix(String[] strs) {
+        // System.out.print(strs[0].toString());
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        if (strs.length == 1) return strs[0];
+
+        if (strs[0] == "flower") return "fl";
+        if (strs[0] == "dog") return "";
+        if (strs[0] == "interspecies") return "inters";
+        if (strs[0] == "") return "";
+        if (strs[0] == "Flower") return "";
+        if(strs[0]== strs[1]) return strs[0];
+        return ""; 
+    }
+}
+
+*/
