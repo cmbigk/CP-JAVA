1348aed54ec36476c8f9410a6ce7fcffd0203504
chmyint <bigk89865@gmail.com>
2025-12-06 18:13:54 +0200

    practicing 4.4

--- IsAnagram/AnagramChecker.java
+++ IsAnagram/AnagramChecker.java
@@ -1,19 +1,17 @@
-import java.util.Arrays;
+
+import java.util.*;
 public class AnagramChecker {
     public boolean isAnagram(String str1, String str2) {
-
-        if (str1.length()== 0 && str2.length()== 0) return true;
-
+        if (str1.length() == 0 && str2.length() == 0) return true;
+        
         String cleanStr1 = str1.toLowerCase();
         String cleanStr2 = str2.toLowerCase();
-
+        
         char[] arr1 = cleanStr1.toCharArray();
         Arrays.sort(arr1);
+        
         char[] arr2 = cleanStr2.toCharArray();
         Arrays.sort(arr2);
-
         return Arrays.equals(arr1,arr2);
-
-    }
-
+        }
 }
