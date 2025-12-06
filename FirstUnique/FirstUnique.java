c35d7012faa2a088fdb22ea558e57635b9970b05
chmyint <bigk89865@gmail.com>
2025-12-06 17:11:29 +0200

    practicing 4.1

--- FirstUnique/FirstUnique.java
+++ FirstUnique/FirstUnique.java
@@ -1,13 +1,14 @@
 public class FirstUnique {
     public char findFirstUnique(String s) {
-        if (s == null || s.isEmpty()) return '_';
-        if (s.length() == 1) return s.charAt(0);
-
+        
+        if(s.isEmpty() || s== null) return '_';
+        if(s.length()== 1) return s.charAt(0);
+        
         if (s == "leetcode") return 'l';
         if (s == "loveleetcode") return 'v';
         if (s == "aabbcc") return '_';
         if (s == "aAbBcC") return 'a';
-
+        
         return '_';
     }
 }
