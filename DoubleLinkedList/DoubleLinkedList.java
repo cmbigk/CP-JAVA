afddcad2c3ea6ad8bb8e4a4e91301265075a5ebd
chmyint <bigk89865@gmail.com>
2025-12-06 03:11:20 +0200

    practicing 3.4

--- DoubleLinkedList/DoubleLinkedList.java
+++ DoubleLinkedList/DoubleLinkedList.java
@@ -1,95 +1,94 @@
 public class DoubleLinkedList implements LinkedList {
     private Node head;
     private Node tail;
-    private int size;
+	private int size;
 
     private class Node {
         int value;
         Node next;
         Node prev;
         
-        Node (int value){
-            this.value = value;
-        }
-    }
+		Node (int value){
+			this.value = value;
+		}
+	}
+
+	public Node goToIndex(int index){
+		Node current ;
+		int step = index;
+		if (index == 0){
+			current = head;
+		}else if (index == size -1){
+			current = tail;
+		}else if ( index <= (size - 1)/2){
+			current = head;
+			for (int i = 0; i < step; i++){
+				current = next(current);
+			}
+		}else{
+			current = tail;
+			step = (size - 1) - index;
+
+			for ( int i = 0; i < step; i++){
+				current = prev(current);
+			}
+		}
+		return current;
+	}
 
 
-    public Node gotoNodeIndex(int index){
-        Node current;
-        int step = index;
-
-        if (index == 0){
-            current = head;
-        }else if (index == size - 1){
-            current = tail;
-        }else if ( index <= ((size - 1)/2)){
-            current = head;
-            for (int i = 0; i < step; i++){
-                current = next(current);
-            }
-        }else{
-            current = tail;
-            step = (size - 1) - index;
-            for (int i = 0; i < step; i++){
-                current = prev(current);
-            }
-        } 
-        return current;
-    }
-    
     @Override
     public int at(int index) {
-        if (index < 0 || index >= size) return -1;
-        Node current = gotoNodeIndex(index);
-        return current.value;
+		if (index < 0 || index >= size) return -1;
+
+		Node current = goToIndex(index);
+		return current.value;
     }
 
     @Override
     public void add(int value) {
-        Node newNode = new Node(value);
-
-        if (head == null){
-            head = tail = newNode;
-        }else{
-            tail.next = newNode;
-            newNode.prev = tail;
-            tail = newNode;
-        }
-        size++;
-        
+		Node newNode = new Node(value);
+		if (head == null){
+			head = tail = newNode;
+		}else{
+			tail.next = newNode;
+			newNode.prev = tail;
+			tail= newNode;
+		}
+		size++;
     }
 
     @Override
     public void remove(int index) {
-        if (index < 0 || index >= size) return;
-        Node current = gotoNodeIndex(index);
+		Node current = goToIndex(index);
+		if (index < 0 || index >= size) return;
 
-        if (current.prev!= null){
-            current.prev.next = current.next;
-        }else{
-            head = current.next;
-        }
+		if(current.prev!= null){
+			current.prev.next= current.next;
+		}else{
+			head = current.next;
+		}
 
-        if (current.next!= null){
-            current.next.prev = current.prev;
-        }else{
-            tail = current.prev;
-        }
-        size--;
+		if (current.next != null){
+			current.next.prev = current.prev;
+		}else{
+			tail = current.prev;
+		}
+		size--;
     }
 
     @Override
     public int size() {
-        return size;
+		return size;
     }
 
     private Node next(Node node) {
-       System.out.println("Go to next node");
-       return node.next;
+        System.out.println("Go to next node");
+		return node.next;
     }
 
     private Node prev(Node node) {
-       System.out.println("Go to previous node");
-       return node.prev;
+        System.out.println("Go to previous node");
+		return node.prev;
     }
 }
