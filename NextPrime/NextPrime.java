c68589d2da5bc5126dc048a8a963d4cc811b9e6e
chmyint <bigk89865@gmail.com>
2026-01-18 21:02:07 +0200

    fixed the error

--- NextPrime/NextPrime.java
+++ NextPrime/NextPrime.java
@@ -3,7 +3,7 @@
 public class NextPrime {
     public static Integer nextPrime(Integer n) {
 
-     if (n < 0) return 0;
+   
      int temp = n+1;
      while (!isPrime(temp)){
         temp++;
@@ -12,7 +12,7 @@ public class NextPrime {
     }
 
     public static boolean isPrime(int x){
-        if ( x <= 1) return false;
+        if ( x <2) return false;
         
         for (int i= 2; i<= Math.sqrt(x);i++){
             if (x % i == 0) return false;
